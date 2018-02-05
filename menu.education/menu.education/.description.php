5c49b05c84be11dcfd8a8392c2d1f26ed1a804db
Antipow <antipow@gmail.com>
2018-02-05 11:28:31 +0300

    v1.0.2

--- menu.education/menu.education/.description.php
+++
@@ -1,6 +0,0 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-$arComponentDescription = array(
-    "NAME"        => "Подменю блока обучения",
-    "DESCRIPTION" => "Список услуг блока обучения для главного меню",
-);
-?>
